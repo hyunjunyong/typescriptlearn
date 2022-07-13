d878a603f705219bdfc2e8502b1ec5d6e3a624de
현준용 <hjy4649@cocen.com>
2022-07-14 08:05:11 +0900

    union type

--- example/7_union-intersection.ts
+++ example/7_union-intersection.ts
@@ -1,17 +1,24 @@
-function logMessage(value: string) {
-  console.log(value);
-}
-function logMessage(value: number) {
-  console.log(value);
-}
-function logMessage(value: any) {
-  console.log(value);
-}
+// function logMessage(value: string) {
+//   console.log(value);
+// }
+// function logMessage(value: number) {
+//   console.log(value);
+// }
+// function logMessage(value: any) {
+//   console.log(value);
+// }
 
 // # Union 타입 문법 - `any` 보다는 명시적임
-function logMessage(value: string | number) {
-  console.log(value);
-}
+// 그동안 하나만 선언할 수 있던것과 달리 union타입은 여러개 타입을 선언가능
+
+var seho: string | number |boolean;
+// function logMessage(value: string | number) {
+//   console.log(value);
+// }
+
+//string| number 이므로 헬로,3 모두 no error
+logMessage('hello')
+logMessage(3);
 
 function logMessage(value: string | number) {
   if (typeof value === 'string') {
