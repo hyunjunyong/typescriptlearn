b11541c9cc1719eedf361fc82ba875d92b111330
현준용 <hjy4649@cocen.com>
2022-11-06 15:57:32 +0900

    emit 까지지

--- vuets/src/App.vue
+++ vuets/src/App.vue
@@ -1,14 +1,31 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <h1>vue todo with typescript</h1>
+    <div>
+      <todoInput :item="todoText" @input="updateTodoText()" />
+    </div>
     <router-view />
   </div>
 </template>
 
+<script lang="ts">
+import Vue from 'vue';
+import todoInput from './components/todoInput.vue';
+export default {
+  name: 'name',
+  components: { todoInput },
+  data() {
+    return {
+      todoText: 'asdf',
+    };
+  },
+  methods: {
+    updateTodoText(value: any) {
+      this.todoText = value;
+    },
+  },
+};
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
---
+++ vuets/src/components/todoInput.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="name">
+    <label for="todo-input"></label>
+    <input id="todo-input" type="text" :value="item" @input="handleInput()" />
+    <button @click="addTodo()" type="button">add</button>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'name',
+  props: {
+    item: String,
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    handleInput(event: any) {
+      event.target.value;
+      this.$emit('input', event.target.value);
+    },
+    addTodo() {
+      console.log('add');
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
